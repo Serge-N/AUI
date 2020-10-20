58d5cc582d95299d8bc635ee888cac2aca757f10
Serge Nalishiwa <51508252+Serge-N@users.noreply.github.com>
2020-10-20 11:45:45 +0200

    Description

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace AUI
 
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://agrixemapi.azurewebsites.net/") });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44325/") });
             
             builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 
